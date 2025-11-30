98fa0f3001e477e5434d3a5c7975cebc58995f4b
guilhermeperas <pguilherme926@gmail.com>
2025-11-30 23:40:32 +0000

    config json

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from click import echo
 from flask import Blueprint, render_template, request
 
+from app.services.service import fetch_data
+
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
+    fetch_data()
     return render_template("index.html")
