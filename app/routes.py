a42bbe96d3abb760384948a412808f8bac1a4e6b
guilhermeperas <pguilherme926@gmail.com>
2025-12-01 17:05:25 +0000

    front-end

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from click import echo
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 
 from app.services.service import fetch_data
 
 main = Blueprint("main", __name__)
 
+
 @main.route("/")
 def index():
-    fetch_data()
-    return render_template("index.html")
+    news = fetch_data()
+    return render_template("index.html", **news)
